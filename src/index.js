7b7598634299df56d2ec8b58607c81e160240df5
Alok Rawat <alokrawatt@gmail.com>
2021-11-28 16:38:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <> 
+      <h1> Alok's Favorite Netflix series!</h1>
+      <p>List of 5 favorite series-</p>
+      <ol>
+        <li>Breaking Bad</li>
+        <li>Stranger Things</li>
+        <li>Brooklyn Nine Nine</li>
+        <li>You</li>
+        <li>Sex Educatiojn</li>
+      </ol>
+      </>,
+  document.getElementById("root"));
